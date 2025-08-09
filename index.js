b3b05c6989b850af40ac31569982abdaac39ef83
TX-SOLDIER <rickm.military@gmail.com>
2025-08-08 22:05:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
+const fetch = require('node-fetch'); // <== added this line
 const express = require('express');
 
 const app = express();
