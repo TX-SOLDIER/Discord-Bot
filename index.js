a4c279c4702a9f9342f587b0b341453df4f9bd08
TX-SOLDIER <rickm.military@gmail.com>
2025-09-30 02:34:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1607,7 +1607,7 @@ if (command === 'help') {
       `• Normal Users: Can only pay coins`
     )
     .setFooter({ text: 'Use $help for full command list 📖' })
-    .setImage('https://i.imgur.com/lhT8tLM.gif'); // permanent image
+    .setImage('https://i.imgur.com/NAneRS5.gif'); // permanent image
 
   // Send embeds
   await message.channel.send({ embeds: [embed1] });
